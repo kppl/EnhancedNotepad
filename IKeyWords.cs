7757605a7276f7ac3f47e6175e362ca2f5652b09
Krzysztof Pasierbinski <pasierbinski@gmail.com>
2020-06-23 08:12:20 +0200

    Initial commit

---
+++ IKeyWords.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedNotepad
+{
+    interface IKeyWords
+    {
+        void GenerateLabel();
+    }
+}
---
+++ KeyWordFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedNotepad
+{
+    public static class KeyWordFactory
+    {
+        static List<string> KeyWords; 
+    }
+}
